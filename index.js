58e84ba0e506c6e59fbe08f329b7b5da9337d502
Lee Vang <vanglee.20@gmail.com>
2023-04-11 15:35:07 -0500

    working collision function

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-let updateId
-let obstacleId
+let updateId;
+let obstacleId;
 
-let speed = 0;
+let score = 0;
 let obstacles = [];
-
 
 const road = new Image();
 road.src = "./images/roadMap.png";
@@ -14,12 +13,18 @@ road.src = "./images/roadMap.png";
 const potholeObst = new Image();
 potholeObst.src = "./images/Pothole.png";
 
+const car = new Image()
+car.src = "./images/car.png";
+
+
 //CLASS CAR=======================================================================================
 class Car {
   //class constructor
   constructor() {
     this.x = 385;
     this.y = 530;
+    this.width = 55;
+    this.height = 61;
     const car = new Image();
     car.addEventListener("load", () => {
       this.car = car;
@@ -48,42 +53,36 @@ class Car {
     }
   }
   draw() {
-    ctx.drawImage(this.car, this.x, this.y, 60, 66);
+    ctx.drawImage(this.car, this.x, this.y, this.width, this.height);
   }
 }
 
 // CLASS POTHOLES OBSTACLES=======================================================================
 
 class Potholes {
-  constructor(){
-    this.x = Math.floor(Math.random() * 500);
+  constructor() {
+    this.x = Math.floor(Math.random() * 400) + 180;
     this.y = 0;
-    this.width = 50 + Math.floor(Math.random() * 110);
-    this.height = 50 + Math.floor(Math.random() * 110);
+    this.width = 20 + Math.floor(Math.random() * 100);
+    this.height = 20 + Math.floor(Math.random() * 100);
   }
 
   //methods
 
   moveDown() {
-    // console.log('this string moving down')
     this.y += 1;
   }
-  
-  draw(){
-    // console.log('this is line 71')
+
+  draw() {
     ctx.drawImage(potholeObst, this.x, this.y, this.width, this.height);
   }
-
-
 }
 //================================================
-//const gameObstacle = new Potholes();
+
 const playerCar = new Car();
+// console.log(playerCar)
 
-
-//UPDATE GAME===================================================================
-
-
+//UPDATE GAME FUNCTION===================================================================
 function updateGame() {
   // speed += 1
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -94,34 +93,64 @@ function updateGame() {
       // console.log('obstacles', obstacles)
       obstacles[i].moveDown();
       obstacles[i].draw();
-      if(obstacles[i].y > canvas.height) {
-        obstacles.splice(i, 1)
+      carCollision(obstacles[i]);
+      if (obstacles[i].y > canvas.height) {
+        obstacles.splice(i, 1);
+        console.log('detected')
       }
     }
   }
-  
 }
-
+//GENERATE OBSTACLES FUNCTION===============================================================
 function generateObstacles() {
-  obstacles.push(new Potholes())
+  obstacles.push(new Potholes());
 }
 //START GAME FUNCTION=======================================================================
-
 function startGame() {
   // ctx.drawImage(road, 0, 0, 800, 600);
   // playerCar.draw();
 
   updateId = setInterval(updateGame, 20);
 
-  obstacleId = setInterval(generateObstacles, 2000);
-
+  obstacleId = setInterval(generateObstacles, 1500);
 }
+//GAME OVER FUNCTION======================================================
+function gameOver() {
+  clearInterval(updateId);
+  clearInterval(obstacleId);
+  // ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // ctx.fillStyle = "black";
+  // ctx.fillRect(0, 0, canvas.width, canvas.height);
+  // ctx.fillStyle = "white";
+  // ctx.font = "20px Arial";
 
-//GAME OVER FUNCTION=======================================================================
-//COLLISION FUNCTION=======================================================================
-//SCORE FUNCTION=======================================================================
-
-//EVENT LISTENER=======================================================================
+  if (score < 5) {
+    ctx.fillText("You died", 450, 250);
+    ctx.font = "32px Arial";
+    ctx.fillText(`Final Score: ${score}`, 450, 350);
+  } else {
+    ctx.fillText("You win!", 450, 250);
+    ctx.font = "32px Arial";
+    ctx.fillText(`Final Score: ${score}`, 450, 350);
+  }
+}
+//COLLISION FUNCTION==========================================================
+function carCollision(obst) {
+  if (
+    playerCar.x < obst.x + obst.width &&
+    playerCar.x + playerCar.width > obst.x &&
+    playerCar.y < obst.y + obst.height &&
+    playerCar.y + playerCar.height > obst.y
+  ) {
+    gameOver();
+    console.log("game over");
+  }
+}
+//SCORE FUNCTION===========================================================
+function finalScore() {
+  
+}
+//EVENT LISTENER==========================================================
 document.addEventListener("keydown", (e) => {
   switch (e.keyCode) {
     case 37:
